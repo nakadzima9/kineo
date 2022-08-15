e490765ccde91e907063d432874b6e8433d46144
Nakadzima <pascal.german@yandex.ru>
2022-08-15 19:06:37 +0600

    write movie models

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=100, verbose_name='Название фильма')
+    movie_description = models.TextField(verbose_name='Описание фильма')
+    movie_startProduction = models.DateTimeField(auto_now_add=True)
+    movie_genre = models.ForeignKey('Genre', on_delete=models.CASCADE, verbose_name='Жанр')
+
+
+class Genre(models.Model):
+    genre_title = models.CharField(max_length=42)
+    genre_description = models.TextField()
