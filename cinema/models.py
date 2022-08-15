0648a465202fa37b1dd80ff7b0c08a6327edf476
Nakadzima <pascal.german@yandex.ru>
2022-08-15 19:05:35 +0600

    write cinema models

--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Cinema(models.Model):
+    cinema_title = models.CharField(max_length=255, verbose_name='Название кинотеатра')
+    cinema_description = models.TextField(verbose_name='Описание кинотеатра')
+    cinema_startTime = models.DateTimeField(auto_now_add=True)
+    cinema_endTime = models.DateTimeField(auto_now_add=True)
+    cinema_address = models.CharField(max_length=255)
+    cinema_website = models.URLField(max_length=200, null=True, blank=True)
