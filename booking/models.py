78f7799fe39b14114db15b7122e96db89067587b
Nakadzima <pascal.german@yandex.ru>
2022-08-16 13:01:38 +0600

    booking app

---
+++ booking/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+class Ticket(models.Model):
+    pass
+
+
+class Booking(models.Model):
+    pass
+
+
+class BookingHistory(models.Model):
+    pass
+
+
+class PayMethod(models.Model):
+    pass
+
+
+class TicketType(models.Model):
+    pass
