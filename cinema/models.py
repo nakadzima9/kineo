061cde6d470159fc26f99aadcedcd61e617ec4df
Nakadzima <pascal.german@yandex.ru>
2022-08-19 14:54:11 +0600

    add models to cinema app

--- cinema/models.py
+++ cinema/models.py
@@ -8,3 +8,11 @@ class Cinema(models.Model):
     cinema_endTime = models.DateTimeField(auto_now_add=True)
     cinema_address = models.CharField(max_length=255, verbose_name='Адрес кинотеатра')
     cinema_website = models.URLField(max_length=200, null=True, blank=True)
+
+
+class Room(models.Model):
+    pass
+
+
+class ShowTime(models.Model):
+    pass
