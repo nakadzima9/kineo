6953b140498021f15c61dd93a9eb31b06ab44be6
Nakadzima <pascal.german@yandex.ru>
2022-08-19 14:54:31 +0600

    add models to movie app

--- movie/models.py
+++ movie/models.py
@@ -8,6 +8,14 @@ class Movie(models.Model):
     movie_genre = models.ForeignKey('Genre', on_delete=models.CASCADE, verbose_name='Жанр')
 
 
+class Director(models.Model):
+    director_name = models.CharField(max_length=255, verbose_name='Режиссёр')
+    director_career = models.TextField()
+    director_date_of_birth = models.CharField(max_length=100, verbose_name='Дата рождения')
+    director_place_of_birth = models.CharField(max_length=100, verbose_name='Место рождения')
+    movie_director = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+
 class Genre(models.Model):
     genre_title = models.CharField(max_length=42)
     genre_description = models.TextField()
