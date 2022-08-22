32e6703ed96fce691476c702454b74a13dc5ba3f
Nakadzima <pascal.german@yandex.ru>
2022-08-22 23:21:09 +0600

    fix register

--- users/serializers.py
+++ users/serializers.py
@@ -12,7 +12,7 @@ class UserSerializer(serializers.ModelSerializer):
             "username",
             "password",
         ]
-        extra_kwargs = {"password": {"write_only", True}}
+        extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
         user = User.objects.create(
